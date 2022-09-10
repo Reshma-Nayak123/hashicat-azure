3cb453a826ddcca4be61af2c5c95e9c059a32016
Reshma-Nayak123 <106798480+Reshma-Nayak123@users.noreply.github.com>
2022-09-10 14:22:14 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/INSTRUQT1/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name ="group1"
+  resource_group_name ="reshma-workshop"
 }
